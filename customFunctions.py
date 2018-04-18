623b9379d4d55fb49ab0727537953b15ccf4ce18
Daniel MacMillan <dmacmillan@dmacmillan02.phage.bcgsc.ca>
2018-04-18 12:56:20 -0700

    Refactored pslToGff

--- customFunctions.py
+++ customFunctions.py
@@ -1,5 +1,4 @@
 import os
-import logging
 import gzip
 try:
     maketrans = ''.maketrans
@@ -81,7 +80,7 @@ def translate(dna, codon_dict, truncate = True):
     aa = ''
     seqlen = len(dna)
     if seqlen % 3 != 0:
-        logging.warning('Sequence of length {} is not divisible by 3'.format(seqlen))
+        # logging.warning('Sequence of length {} is not divisible by 3'.format(seqlen))
         if truncate:
             dna = dna[:-(seqlen % 3)]
             seqlen = len(dna)
@@ -161,106 +160,6 @@ def get_file_type(_file):
             return 'fasta', gzipped
         elif fxn(line[0]) == '@':
             return 'fastq', gzipped
-
-class Gff:
-
-    # GFF format columns
-    cols = (
-        'seqname',
-        'source',
-        'feature',
-        'start',
-        'end',
-        'score',
-        'strand',
-        'frame',
-        'attribute'
-    )
-
-    def __init__(
-        self,
-        seqname = 'NA',
-        source = 'NA',
-        feature = 'NA',
-        start = 'NA',
-        end = 'NA',
-        score = 'NA',
-        strand = 'NA',
-        frame = 'NA',
-        attribute = 'NA'
-    ):
-        import re
-        pattern = re.compile(r'\s*;\s*')
-        self.seqname = seqname
-        self.source = source
-        self.feature = feature
-        self.start = start
-        self.end = end
-        self.score = score
-        self.strand = strand
-        self.frame = frame
-        self.start = self.toInt(start)
-        self.end = self.toInt(end)
-        self.score = self.toInt(score)
-        self.frame = self.toInt(frame)
-        try:
-            self.attribute = dict([x.split('=') for x in re.split(pattern, attribute)])
-        except ValueError:
-            pass
-
-    def __str__(self):
-        try:
-            attribute = ('; ').join((
-                '{}={}'.format(key, value) for key, value in self.attribute.items()
-            ))
-        except AttributeError:
-            attribute = 'NA'
-        return ('\t').join((
-            self.seqname,
-            self.source,
-            self.feature,
-            str(self.start),
-            str(self.end),
-            str(self.score),
-            self.strand,
-            str(self.frame),
-            attribute
-            )
-        )
-
-    @staticmethod
-    def toInt(string):
-        try:
-            return int(string)
-        except ValueError:
-            return string
-
-    @staticmethod
-    def read(gff_file):
-        # Required libraries
-        import re
-        pattern = re.compile(r'\s*;\s*')
-        with open(gff_file, 'r') as f:
-            for line in f:
-                # Skip commented lines
-                if line[0] == '#':
-                    continue
-                cols = line.strip().split('\t')
-                try:
-                    seqname, source, feature, start, end, score, strand, frame, attribute = cols
-                except ValueError:
-                    raise
-                # Try to load the attributes into a dictionary
-                try:
-                    attributes = dict([x.split('=') for x in re.split(pattern, cols[8])])
-                except Exception as e:
-                    raise
-                # Convert integer columns
-                try:
-                    start, end, score, frame = map(int, (start, end, score, frame))
-                except ValueError:
-                    pass
-                yield Gff(seqname, source, feature, start, end, score, strand, frame, attribute)
 
 def parse_gmap_gff(_file):
     with open(_file, 'r') as f:
--- getErrorRatesFromSam.py
+++ getErrorRatesFromSam.py
@@ -1,10 +1,10 @@
+__version__ = '1.0'
+
 import argparse
 import os
 import sys
 import re
 import logging
-import scipy.stats
-import numpy as np
 
 output_columns = (
     'qname',
@@ -13,29 +13,58 @@ output_columns = (
     'seq_matches',
     'seq_mismatches',
     'insertions',
-    'deletions'
+    'deletions',
+    'clips'
 )
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description = 'Given a SAM file, return a list of percent identities ' \
-                      'for each alignment based on the cigar string and NM tag',
+                      'for each alignment based on the cigar string and NM tag. ' \
+                      'Warning: this script assumes that given multiple alignments ' \
+                      'for the same query that the best alignment is first.',
         formatter_class = argparse.ArgumentDefaultsHelpFormatter
     )
-    
+
     parser.add_argument('sam', help='SAM file input')
+
     parser.add_argument(
         '-n',
         '--name',
         help = 'Set the name of the file. Otherwise will be basename of sam ' \
                'appended with ".rates"'
     )
+
     parser.add_argument(
         '-o',
         '--outdir',
         default = os.getcwd(),
         help = 'Directory to output to, will be created if it does not exist'
     )
+
+    parser.add_argument(
+        '-b',
+        '--best',
+        action = 'store_false',
+        help = 'Set this flag to disable selecting only the first alignment per read'
+    )
+
+    parser.add_argument(
+        '-mcs',
+        '--max_clip_start',
+        type = int,
+        default = '200',
+        help = 'Filter any alignments that have a clipped region exceeding this value at the start of the sequence'
+    )
+
+    parser.add_argument(
+        '-mce',
+        '--max_clip_end',
+        type = int,
+        default = '200',
+        help = 'Filter any alignments that have a clipped region exceeding this value at the end of the sequence'
+    )
+
     parser.add_argument(
         '-l',
         '--log_level',
@@ -50,7 +79,7 @@ if __name__ == '__main__':
         default = 'error',
         help='Logging level'
     )
-    
+
     args = parser.parse_args()
 
     if not args.name:
@@ -62,8 +91,10 @@ if __name__ == '__main__':
     logging.basicConfig(
         level = getattr(logging, args.log_level.upper())
     )
-    
+
     output_file = os.path.join(args.outdir, args.name)
+
+    seen = set()
 
     with open(output_file, 'w') as o:
         o.write(
@@ -74,12 +105,17 @@ if __name__ == '__main__':
             ) + '\n'
         )
         with open(args.sam, 'r') as f:
-            for line in f:
+            for i,line in enumerate(f):
+                # Skip header lines
                 if line[0] == '@':
                     continue
                 output_dict = {x:None for x in output_columns}
                 cols = line.strip().split('\t')
                 qname = cols[0]
+                if args.best:
+                    if qname in seen:
+                        continue
+                    seen.add(qname)
                 output_dict['qname'] = qname
                 flag = int(cols[1])
                 cigar = cols[5]
@@ -88,16 +124,16 @@ if __name__ == '__main__':
                 output_dict['qlen'] = qlen
                 # Check if sequence is aligned
                 if flag & 4:
-                    logging.warning('"{}" is unmapped!'.format(qname))
+                    logging.debug('"{}" is unmapped!'.format(qname))
                     continue
                 # Sequence is a secondary alignment
                 elif flag & 256:
-                    logging.warning('"{}" is a secondary alignment!'.format(qname))
+                    logging.debug('"{}" is a secondary alignment!'.format(qname))
                     continue
                 ## 2018-01-10
                 # Sequence is a supplementary alignment
                 elif flag & 2048:
-                    logging.warning('"{}" is a supplementary alignment!'.format(qname))
+                    logging.debug('"{}" is a supplementary alignment!'.format(qname))
                     continue
                 ##
                 # According to my research, the NM tag represents
@@ -111,10 +147,10 @@ if __name__ == '__main__':
                     elif item[:2] == 'NM':
                         NM = int(item[5:])
                 if not NM:
-                    logging.warning('"{}" has no NM tag'.format(qname))
+                    logging.warning('"{}" has no NM tag! Continuing'.format(qname))
                     continue
                 if cigar == '*':
-                    logging.warning('"{}" is mapped, but cigar is "{}"'.format(qname, cigar))
+                    logging.warning('"{}" is mapped, but cigar is "{}". Continuing!'.format(qname, cigar))
                     continue
                 cigar_dict = {
                     'M': 0,
@@ -127,10 +163,25 @@ if __name__ == '__main__':
                     '=': 0,
                     'X': 0
                 }
-                for item in re.findall(r'\d+[A-Z]', cigar):
+                clipped_start = clipped_end = 0
+                seen_non_clipped = False
+                for i,item in enumerate(re.findall(r'\d+[A-Z]', cigar)):
                     op = item[-1]
                     num = int(item[:-1])
+                    if (op in ('S', 'H')):
+                      if (not seen_non_clipped):
+                        clipped_start += num
+                      else:
+                        clipped_end += num
+                    else:
+                      seen_non_clipped = i
                     cigar_dict[op] += num
+                if (clipped_start > args.max_clip_start):
+                  logging.debug('"{}" has clipped start = "{}" > "{}"'.format(qname, clipped_start, args.max_clip_start))
+                  continue
+                elif (clipped_end > args.max_clip_end):
+                  logging.debug('"{}" has clipped end = "{}" > "{}"'.format(qname, clipped_end, args.max_clip_end))
+                  continue
                 output_dict['aln_matches'] = cigar_dict['M']
                 # The number of matches is computed as follows:
                 output_dict['seq_matches'] = cigar_dict['M'] + \
@@ -141,6 +192,8 @@ if __name__ == '__main__':
                 output_dict['seq_mismatches'] = cigar_dict['M'] - output_dict['seq_matches']
                 output_dict['insertions'] = cigar_dict['I']
                 output_dict['deletions'] = cigar_dict['D']
+                output_dict['clips'] = cigar_dict['H'] + cigar_dict['S']
+                logging.info('Successfully extracted line {}'.format(i))
                 o.write(
                     ('\t').join(
                         [
@@ -148,19 +201,3 @@ if __name__ == '__main__':
                         ]
                     ) + '\n'
                 )
-#            mismatch_rate = nmismatches / float(cigar_dict['M'])
-#            insertion_rate = cigar_dict['I'] / float(cigar_dict['M'])
-#            deletion_rate = cigar_dict['D'] / float(cigar_dict['M'])
-
-#if not args.read_lengths:
-#    for i in range(len(tmat)):
-#        print 100 * (tmat[i] + tmis[i] - tnm[i]) / float(tmat[i])
-#    mmis = mean(tmis)
-#    mins = mean(tins)
-#    mdel = mean(tdel)
-#    sys.stderr.write('Average mismatch rate: {}\n'.format(mmis))
-#    sys.stderr.write('Average insertion rate: {}\n'.format(mins))
-#    sys.stderr.write('Average deletion rate: {}\n'.format(mdel))
-#    sys.stderr.write('Total average error rate: {}\n'.format(mmis + mins + mdel))
-#    
-#    #print scipy.stats.describe(tmis)
